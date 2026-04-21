0fd693277a93e8a5a905197abb2b20393b1b4676
Elk Cloner <dev@elkcl.ru>
2026-04-21 22:17:28 +0300

    unzstd instead of copying in execute.sh

--- execute.sh
+++ execute.sh
@@ -8,31 +8,30 @@ if [ $# -lt 1 ]; then
     die "not enough arguments. usage: $0 <dir>"
 fi
 
-TMPDIR=$(mktemp -d)
 MOUNTPOINT=/mnt
 LOOPDEVICE=/dev/loop0
     
 echo "Resetting llvm-cov..."
 echo 1 > /sys/kernel/debug/llvm-cov/reset
 
-for f in "$1"/*.img; do {
+for f in "$1"/*.img.zst; do {
     echo "Tesing $f..."
     echo "Compiling..."
-    gcc -Wall --std=gnu99 "${f%.img}.c" -o "${f%.img}.test"
-    echo "Copying image..."
-    cp -f "$f" "$TMPDIR/curr.img"
+    gcc -Wall --std=gnu99 "${f%.img.zst}.c" -o "${f%.img.zst}.test"
+    echo "Decompressing image..."
+    unzstd "$f"
     echo "Setting up loop device..."
-    losetup $LOOPDEVICE "$TMPDIR/curr.img"
+    losetup $LOOPDEVICE "${f%.zst}"
     echo "Mounting..."
     mount $LOOPDEVICE $MOUNTPOINT
     echo "Testing..."
-    setpriv --bounding-set '-dac_override' "${f%.img}.test" $MOUNTPOINT
+    setpriv --bounding-set '-dac_override' "${f%.img.zst}.test" $MOUNTPOINT
     echo "Unmounting..."
     umount $MOUNTPOINT
     echo "Resetting loop device..."
     losetup -d $LOOPDEVICE
     echo "$f done!"
-} 2>&1 | tee -a "${f%.img}.log"
+} 2>&1 | tee -a "${f%.img.zst}.log"
 done
 
 # echo "Collecting coverage info..."
